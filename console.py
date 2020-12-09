bac84aaca04ce35b0eb2302714a94373cc0d1751
Bela <belahamid@gmail.com>
2020-12-09 09:52:00 +0000

    added delete_all() to artists repository

--- console.py
+++ console.py
@@ -5,9 +5,17 @@ import repositories.album_repository as album_repository
 import repositories.artist_repository as artist_repository
 
 album_repository.delete_all()
+artist_repository.delete_all()
 
 album1 = Album("Cruelty and the Beast", "Cradle of Filth", "Metal" )
 album_repository.save(album1)
+album2 = Album("Lemonade", "Beyonce", "Pop")
+album_repository.save(album2)
 
 artist1 = Artist("Cradle of Filth")
 artist_repository.save(artist1)
+artist2 = Artist("Beyonce")
+artist_repository.save(artist2)
+
+
+
--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -3,9 +3,7 @@ from models.artist import Artist
 
 
 # Create and Save Artists
-# Delete all Artists
-# Find Artists by their ID (select)
-# List All Artists (select_all)
+
 def save(artist):
     sql = "INSERT INTO artists (name) VALUES (%s) RETURNING *"
     values = [artist.name]
@@ -13,6 +11,15 @@ def save(artist):
     id = results[0]['id']
     artist.id = id
     return artist
+
+
+# Delete all Artists
+
+def delete_all():
+    sql = "DELETE FROM artists"
+    run_sql(sql)
+# Find Artists by their ID (select)
+# List All Artists (select_all)
 
 
 
